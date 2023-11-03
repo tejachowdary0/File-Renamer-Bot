eb1bc45d6a66b26128870273e9eab5c4351321a8
tejachowdary0 <141829994+tejachowdary0@users.noreply.github.com>
2023-11-04 03:17:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,22 +2,22 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "27639102")
+API_ID = os.environ.get("API_ID", "27604683")
 
-API_HASH = os.environ.get("API_HASH", "35142c1407be6264e68fb6bec5dcabd9")
+API_HASH = os.environ.get("API_HASH", "ed52a1d0803b2ed84c5cca7f20535aac")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5906887802:AAFxiHDzneMSKc70V9JNgUufx2_GpCE08Dw") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6636903897:AAH4jTAphNyRH9jHXQfi-FQd3WO-3LSRcxU") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "-1001787446188") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "") 
 
-DB_NAME = os.environ.get("DB_NAME","renamerbot")     
+DB_NAME = os.environ.get("DB_NAME","jonny002")     
 
-DB_URL = os.environ.get("DB_URL","mongodb+srv://Fileshare123:Fileshare123@cluster0.nqjle5f.mongodb.net/?retryWrites=true&w=majority")
+DB_URL = os.environ.get("DB_URL","mongodb+srv://jonnywaker002:WlPnEIcbNTY2jc47@jonny002.fgsyjqw.mongodb.net/?retryWrites=true&w=majority")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
 START_PIC = os.environ.get("START_PIC", "https://te.legra.ph/file/14929fa56470f5bd74b88.jpg")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5606411877').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1264280791').split()]
 
 PORT = os.environ.get("PORT", "8080")
